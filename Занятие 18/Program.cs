8f5c4badcdf8251a8a5f24a31102b24496d125c6
s.fedorovich <s.fedorovich@CZC71776GT>
2021-11-17 17:10:40 +0300

    Добавьте файлы проекта.

---
+++ Занятие 18/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Занятие_18
+{
+    class Program
+    {
+            static bool IsWellFormedBrackets(string input)
+            {
+                string previous = "";
+                while (input.Length != previous.Length)
+                {
+                    previous = input;
+                    input = input
+                        .Replace("()", String.Empty)
+                        .Replace("[]", String.Empty)
+                        .Replace("{}", String.Empty);
+                }
+                return (input.Length == 0);
+            }
+        
+    }
+}
