e0ea5e220f575e9267b7fe708eccf4020bfbebe3
s.fedorovich <s.fedorovich@CZC71776GT>
2021-11-17 17:12:48 +0300

    Задача

--- Занятие 18/Program.cs
+++ Занятие 18/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Занятие_18
-{
+{// задача на проверку корректности расстановки скобок
     class Program
     {
             static bool IsWellFormedBrackets(string input)
