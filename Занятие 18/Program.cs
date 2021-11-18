6fa9f383e2d9d97ef762f5b9cf9fa2a2978323ef
s.fedorovich <s.fedorovich@CZC71776GT>
2021-11-18 17:21:32 +0300

    задача

--- Занятие 18/Program.cs
+++ Занятие 18/Program.cs
@@ -8,19 +8,6 @@ namespace Занятие_18
 {// задача на проверку корректности расстановки скобок
     class Program
     {
-            static bool IsWellFormedBrackets(string input)
-            {
-                string previous = "";
-                while (input.Length != previous.Length)
-                {
-                    previous = input;
-                    input = input
-                        .Replace("()", String.Empty)
-                        .Replace("[]", String.Empty)
-                        .Replace("{}", String.Empty);
-                }
-                return (input.Length == 0);
-            }
         
     }
 }
